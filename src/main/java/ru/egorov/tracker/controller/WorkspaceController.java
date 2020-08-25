1fd56e1e5497c0ba8b023642fa4ccbdff4f06dfa
vasilijegorov <kot2046@gmail.com>
2020-08-25 21:42:54 +0300

    add Form add userproject

--- src/main/java/ru/egorov/tracker/controller/WorkspaceController.java
+++ src/main/java/ru/egorov/tracker/controller/WorkspaceController.java
@@ -23,12 +23,36 @@ public class WorkspaceController {
     private UserRepo userRepo;
 
     @PostMapping("/workspace")
-    public String inWorkspace(@AuthenticationPrincipal User user, @RequestParam Long id, Model model) {
-        Optional<Project> project = projectRepo.findById(id);
-        Project project1 = project.get();
+    public String inWorkspace(@AuthenticationPrincipal User user, @RequestParam Long projectid, Model model) {
+        Project project = projectRepo.findById(projectid).get();
+        //Project project1 = project.get();
         //Long idp = project.get().getId();
 
-        model.addAttribute("project1", project1);
+        model.addAttribute("project", project);
+
+        Iterable<User> users = userRepo.findAll();
+        model.addAttribute("users", users);
+
+        return "/workspace";
+    }
+
+    /*@GetMapping("/workspace")
+    public String homeWorkspace(@AuthenticationPrincipal User user, @RequestParam Long projectid, Model model) {
+
+        return "workspace";
+    }*/
+
+    @PostMapping("/addProjectUser")
+    public String addProjectUser(@AuthenticationPrincipal User user, @RequestParam Long userid, @RequestParam Long projectid,
+                                 Model model) {
+        User newUser = userRepo.findById(userid).get();
+        Project project = projectRepo.findById(projectid).get();
+        project.getUsers().add(newUser);
+        projectRepo.save(project);
+        newUser.getProjects().add(project);
+        userRepo.save(newUser);
+
+        model.addAttribute("project", project);
 
         Iterable<User> users = userRepo.findAll();
         model.addAttribute("users", users);
