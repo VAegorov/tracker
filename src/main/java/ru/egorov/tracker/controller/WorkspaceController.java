f4298775b351aa5f424156b060911221610bb8ce
EgorovVA <kot2046@gmail.com>
2020-08-30 15:19:51 +0300

    реализован redirect

--- src/main/java/ru/egorov/tracker/controller/WorkspaceController.java
+++ src/main/java/ru/egorov/tracker/controller/WorkspaceController.java
@@ -1,11 +1,14 @@
 package ru.egorov.tracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 import ru.egorov.tracker.domain.Project;
 import ru.egorov.tracker.domain.User;
 import ru.egorov.tracker.domain.issue.Issue;
@@ -15,7 +18,7 @@ import ru.egorov.tracker.repos.IssueRepo;
 import ru.egorov.tracker.repos.ProjectRepo;
 import ru.egorov.tracker.repos.UserRepo;
 
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class WorkspaceController {
@@ -48,9 +51,6 @@ public class WorkspaceController {
 
         isEditor = false;
         if (project.isOwner(user) || project.isAdmin(user)) {
-            System.out.println("user: " + user.toString());
-            System.out.println("owner: " + project.getOwner().toString());
-            System.out.println("admin: " + project.getAdmin().toString());
             isEditor = true;
         }
         model.addAttribute("isEditor", isEditor);
@@ -59,10 +59,10 @@ public class WorkspaceController {
     }
 
     @PostMapping("/addissue")
-    public String addIssue(@AuthenticationPrincipal User user, @RequestParam Long projectid,
-                           @RequestParam String issueName, @RequestParam String issueDescription,
-                           @RequestParam IssuePriority issuePriority, @RequestParam IssueStatus issueStatus,
-                           Model model) {
+    public ModelAndView addIssue(@AuthenticationPrincipal User user, @RequestParam Long projectid,
+                                 @RequestParam String issueName, @RequestParam String issueDescription,
+                                 @RequestParam IssuePriority issuePriority, @RequestParam IssueStatus issueStatus,
+                                 HttpServletRequest request) {
         Project project = projectRepo.findById(projectid).get();
 
         if (!issueName.isEmpty() && !issueDescription.isEmpty()) {
@@ -71,33 +71,15 @@ public class WorkspaceController {
             projectRepo.save(project);
         }
 
-        Iterable<Issue> issues = issueRepo.findAllByProjectId(projectid);
-        model.addAttribute("issues", issues);
-        model.addAttribute("project", project);
+        request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT);
 
-        Iterable<User> users = userRepo.findNewUser(projectid);
-        model.addAttribute("users", users);
-
-        IssuePriority[] issuePriorities = IssuePriority.values();
-        model.addAttribute("issuePriorities", issuePriorities);
-
-        IssueStatus[] issueStatuses = IssueStatus.values();
-        model.addAttribute("issueStatuses", issueStatuses);
-
-        isEditor = false;
-        if (project.isOwner(user) || project.isAdmin(user)) {
-            isEditor = true;
-        }
-        model.addAttribute("isEditor", isEditor);
-
-        return "/workspace";
+        return new ModelAndView("redirect:/workspace");
     }
 
     @PostMapping("/addProjectUser")
-    public String addProjectUser(@AuthenticationPrincipal User user,
+    public ModelAndView addProjectUser(@AuthenticationPrincipal User user,
                                  @RequestParam Long userid,
-                                 @RequestParam Long projectid,
-                                 Model model) {
+                                 @RequestParam Long projectid, HttpServletRequest request) {
         User newUser = userRepo.findById(userid).get();
         Project project = projectRepo.findById(projectid).get();
         project.getUsers().add(newUser);
@@ -105,30 +87,8 @@ public class WorkspaceController {
         newUser.getProjectsUser().add(project);
         userRepo.save(newUser);
 
-        Iterable<Issue> issues = issueRepo.findAllByProjectId(projectid);
-        model.addAttribute("issues", issues);
-        model.addAttribute("project", project);
+        request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT);
 
-        Iterable<User> users = userRepo.findNewUser(projectid);
-        model.addAttribute("users", users);
-
-        IssuePriority[] issuePriorities = IssuePriority.values();
-        model.addAttribute("issuePriorities", issuePriorities);
-
-        IssueStatus[] issueStatuses = IssueStatus.values();
-        model.addAttribute("issueStatuses", issueStatuses);
-
-        isEditor = false;
-        if (project.isOwner(user) || project.isAdmin(user)) {
-            isEditor = true;
-        }
-        model.addAttribute("isEditor", isEditor);
-
-        return "/workspace";
+        return new ModelAndView("redirect:/workspace");
     }
-
-    /*@GetMapping
-    public String workspace() {
-        return null;
-    }*/
 }
